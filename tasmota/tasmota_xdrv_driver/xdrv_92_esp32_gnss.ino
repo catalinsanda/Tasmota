b4682b0ee1bd34b20c97d443830e13ac90db05c5
Catalin Sanda <catalin.sanda@gmail.com>
2025-01-20 10:01:11 +0200

    Add NMEA message statistics in the UI

--- tasmota/tasmota_xdrv_driver/xdrv_92_esp32_gnss.ino
+++ tasmota/tasmota_xdrv_driver/xdrv_92_esp32_gnss.ino
@@ -12,7 +12,7 @@
   You should have received a copy of the GNU General Public License
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-  
+
 #ifdef ESP32
 #ifdef USE_GNSS
 
@@ -26,7 +26,18 @@
 #define D_CMND_GNSS "GNSS"
 #define RTC_UPDATE_INTERVAL (30 * 60) // Every 30 minutes
 #define INVALIDATE_AFTER 30           // 30 seconds
-#define SERIAL_INPUT_BUFFER_SIZE  2048
+#define SERIAL_INPUT_BUFFER_SIZE 2048
+
+struct NMEACounters
+{
+  uint32_t gga;   // Global Positioning System Fix Data
+  uint32_t rmc;   // Recommended Minimum Navigation Information
+  uint32_t gll;   // Geographic Position - Latitude/Longitude
+  uint32_t gsa;   // GNSS DOP and Active Satellites
+  uint32_t gsv;   // GNSS Satellites in View
+  uint32_t vtg;   // Course Over Ground and Ground Speed
+  uint32_t other; // Any other NMEA sentences
+};
 
 struct
 {
@@ -42,6 +53,8 @@ struct
   char time[32];
   char date[32];
   char last_valid_time[32];
+  NMEACounters nmea_counters;
+  uint32_t total_nmea_sentences;
 } GNSSData;
 
 TinyGPSPlus gps;
@@ -120,9 +133,46 @@ void GNSSInit(void)
   }
 }
 
+void UpdateNMEACounter(const char *sentence)
+{
+  GNSSData.total_nmea_sentences++;
+
+  if (strncmp(sentence, "$GPGGA", 6) == 0 || strncmp(sentence, "$GNGGA", 6) == 0)
+  {
+    GNSSData.nmea_counters.gga++;
+  }
+  else if (strncmp(sentence, "$GPRMC", 6) == 0 || strncmp(sentence, "$GNRMC", 6) == 0)
+  {
+    GNSSData.nmea_counters.rmc++;
+  }
+  else if (strncmp(sentence, "$GPGLL", 6) == 0 || strncmp(sentence, "$GNGLL", 6) == 0)
+  {
+    GNSSData.nmea_counters.gll++;
+  }
+  else if (strncmp(sentence, "$GPGSA", 6) == 0 || strncmp(sentence, "$GNGSA", 6) == 0)
+  {
+    GNSSData.nmea_counters.gsa++;
+  }
+  else if (strncmp(sentence, "$GPGSV", 6) == 0 || strncmp(sentence, "$GNGSV", 6) == 0 ||
+           strncmp(sentence, "$GBGSV", 6) || strncmp(sentence, "$GAGSV", 6) || strncmp(sentence, "$GLGSV", 6))
+  {
+    GNSSData.nmea_counters.gsv++;
+  }
+  else if (strncmp(sentence, "$GPVTG", 6) == 0 || strncmp(sentence, "$GNVTG", 6) == 0)
+  {
+    GNSSData.nmea_counters.vtg++;
+  }
+  else
+  {
+    GNSSData.nmea_counters.other++;
+  }
+}
+
 bool ProcessNMEAMessage(const char *nmea_message, uint8_t length)
 {
   AddLog(LOG_LEVEL_DEBUG, PSTR("GNSS: NMEA %s"), nmea_message);
+
+  UpdateNMEACounter(nmea_message);
 
   bool newData = false;
   for (int i = 0; i < length; i++)
@@ -198,111 +248,127 @@ void UpdateLastValidTime(uint32_t current_time)
   }
 }
 
-void LogRawBuffer(const uint8_t* buffer, size_t length) {
-    if (!length) return;
-    
-    const size_t BYTES_PER_LINE = 16;
-    char hex_line[BYTES_PER_LINE * 3 + 1];  // 2 chars per byte + space + null
-    char ascii_line[BYTES_PER_LINE + 1];     // 1 char per byte + null
-    
-    for (size_t offset = 0; offset < length; offset += BYTES_PER_LINE) {
-        size_t line_length = std::min(BYTES_PER_LINE, length - offset);
-        
-        memset(hex_line, ' ', sizeof(hex_line));
-        memset(ascii_line, ' ', sizeof(ascii_line));
-        hex_line[sizeof(hex_line) - 1] = 0;
-        ascii_line[sizeof(ascii_line) - 1] = 0;
-        
-        for (size_t i = 0; i < line_length; i++) {
-            uint8_t byte = buffer[offset + i];
-            
-            snprintf(&hex_line[i * 3], 4, "%02X ", byte);
-            
-            ascii_line[i] = (byte >= 32 && byte <= 126) ? byte : '.';
-        }
-        
-        AddLog(LOG_LEVEL_DEBUG_MORE, PSTR("GNSS Raw [%04X]: %-48s  |%s|"), 
-               offset, hex_line, ascii_line);
+void LogRawBuffer(const uint8_t *buffer, size_t length)
+{
+  if (!length)
+    return;
+
+  const size_t BYTES_PER_LINE = 16;
+  char hex_line[BYTES_PER_LINE * 3 + 1]; // 2 chars per byte + space + null
+  char ascii_line[BYTES_PER_LINE + 1];   // 1 char per byte + null
+
+  for (size_t offset = 0; offset < length; offset += BYTES_PER_LINE)
+  {
+    size_t line_length = std::min(BYTES_PER_LINE, length - offset);
+
+    memset(hex_line, ' ', sizeof(hex_line));
+    memset(ascii_line, ' ', sizeof(ascii_line));
+    hex_line[sizeof(hex_line) - 1] = 0;
+    ascii_line[sizeof(ascii_line) - 1] = 0;
+
+    for (size_t i = 0; i < line_length; i++)
+    {
+      uint8_t byte = buffer[offset + i];
+
+      snprintf(&hex_line[i * 3], 4, "%02X ", byte);
+
+      ascii_line[i] = (byte >= 32 && byte <= 126) ? byte : '.';
     }
+
+    AddLog(LOG_LEVEL_DEBUG_MORE, PSTR("GNSS Raw [%04X]: %-48s  |%s|"),
+           offset, hex_line, ascii_line);
+  }
 }
 
 void GNSSEvery100ms(void)
 {
   uint32_t current_time = Rtc.utc_time;
   static uint8_t read_buffer[SERIAL_INPUT_BUFFER_SIZE];
-  
+
   if (gnssSerial && gnssSerial->available() > 0)
   {
     size_t bytes_available = gnssSerial->available();
     size_t bytes_to_read = std::min(bytes_available, sizeof(read_buffer));
     size_t bytes_read = gnssSerial->readBytes(read_buffer, bytes_to_read);
-    
+
     AddLog(LOG_LEVEL_DEBUG_MORE, PSTR("GNSS: Read %d bytes from serial"), bytes_read);
     LogRawBuffer(read_buffer, bytes_read);
-       
+
     size_t processed = 0;
-    while (processed < bytes_read) {
+    while (processed < bytes_read)
+    {
       size_t parser_space = gnssParser.available_write_space();
-      
-      if (parser_space == 0) {
-        while (gnssParser.available()) {
+
+      if (parser_space == 0)
+      {
+        while (gnssParser.available())
+        {
           auto msg = gnssParser.getMessage();
-          if (msg.type == GNSSParser::Message::Type::NMEA) {
+          if (msg.type == GNSSParser::Message::Type::NMEA)
+          {
             char nmea_message[128];
             uint8_t length = std::min(sizeof(nmea_message) - 1, msg.length);
             strncpy(nmea_message, (char *)msg.data, length);
             nmea_message[length] = 0;
-            
-            if (ProcessNMEAMessage(nmea_message, length)) {
+
+            if (ProcessNMEAMessage(nmea_message, length))
+            {
               UpdateGNSSData(current_time);
             }
           }
         }
-        
+
         parser_space = gnssParser.available_write_space();
-        
-        if (parser_space == 0 && !gnssParser.available()) {
+
+        if (parser_space == 0 && !gnssParser.available())
+        {
           AddLog(LOG_LEVEL_DEBUG, PSTR("GNSS: No valid messages found and no space available, clearing parser"));
           gnssParser.clear();
           parser_space = gnssParser.available_write_space();
         }
-        
-        if (parser_space == 0) {
+
+        if (parser_space == 0)
+        {
           AddLog(LOG_LEVEL_DEBUG, PSTR("GNSS: Parser buffer full, skipping data"));
           break;
         }
       }
-      
+
       size_t chunk_size = std::min(parser_space, bytes_read - processed);
-      
-      if (!gnssParser.encode(read_buffer + processed, chunk_size)) {
+
+      if (!gnssParser.encode(read_buffer + processed, chunk_size))
+      {
         AddLog(LOG_LEVEL_DEBUG, PSTR("GNSS: Failed to encode chunk of %d bytes"), chunk_size);
         break;
       }
-      
+
       processed += chunk_size;
-      
-      while (gnssParser.available()) {
+
+      while (gnssParser.available())
+      {
         auto msg = gnssParser.getMessage();
-        if (msg.type == GNSSParser::Message::Type::NMEA) {
+        if (msg.type == GNSSParser::Message::Type::NMEA)
+        {
           char nmea_message[128];
           uint8_t length = std::min(sizeof(nmea_message) - 1, msg.length);
           strncpy(nmea_message, (char *)msg.data, length);
           nmea_message[length] = 0;
-          
-          if (ProcessNMEAMessage(nmea_message, length)) {
+
+          if (ProcessNMEAMessage(nmea_message, length))
+          {
             UpdateGNSSData(current_time);
           }
         }
       }
     }
-    
+
     chars_received += bytes_read;
-    
-    if ((current_time - GNSSData.last_valid) > INVALIDATE_AFTER) {
+
+    if ((current_time - GNSSData.last_valid) > INVALIDATE_AFTER)
+    {
       GNSSData.valid = false;
     }
-    
+
     UpdateLastValidTime(current_time);
   }
 }
@@ -311,14 +377,22 @@ void GNSSShow(bool json)
 {
   if (json)
   {
-    ResponseAppend_P(PSTR(",\"GNSS\":{\"Valid\":%d,\"DateTime\":\"%s %s\",\"Lat\":%f,\"Lon\":%f,\"Alt\":%f,\"Sat\":%d,\"HDOP\":%f}"),
+    ResponseAppend_P(PSTR(",\"GNSS\":{\"Valid\":%d,\"DateTime\":\"%s %s\",\"Lat\":%f,\"Lon\":%f,\"Alt\":%f,\"Sat\":%d,\"HDOP\":%f,\"NMEA\":{\"Total\":%d,\"GGA\":%d,\"RMC\":%d,\"GLL\":%d,\"GSA\":%d,\"GSV\":%d,\"VTG\":%d,\"Other\":%d}}"),
                      GNSSData.valid,
                      GNSSData.date, GNSSData.time,
                      GNSSData.latitude,
                      GNSSData.longitude,
                      GNSSData.altitude,
                      GNSSData.satellites,
-                     GNSSData.hdop);
+                     GNSSData.hdop,
+                     GNSSData.total_nmea_sentences,
+                     GNSSData.nmea_counters.gga,
+                     GNSSData.nmea_counters.rmc,
+                     GNSSData.nmea_counters.gll,
+                     GNSSData.nmea_counters.gsa,
+                     GNSSData.nmea_counters.gsv,
+                     GNSSData.nmea_counters.vtg,
+                     GNSSData.nmea_counters.other);
 #ifdef USE_WEBSERVER
   }
   else
@@ -334,6 +408,15 @@ void GNSSShow(bool json)
       WSContentSend_PD(PSTR("{s}GNSS Satellites{m}%d{e}"), GNSSData.satellites);
       WSContentSend_PD(PSTR("{s}GNSS HDOP{m}%f{e}"), GNSSData.hdop);
     }
+
+    WSContentSend_PD(PSTR("{s}Total NMEA Sentences{m}%d{e}"), GNSSData.total_nmea_sentences);
+    WSContentSend_PD(PSTR("{s}&nbsp;&nbsp;• GGA Messages{m}%d{e}"), GNSSData.nmea_counters.gga);
+    WSContentSend_PD(PSTR("{s}&nbsp;&nbsp;• RMC Messages{m}%d{e}"), GNSSData.nmea_counters.rmc);
+    WSContentSend_PD(PSTR("{s}&nbsp;&nbsp;• GLL Messages{m}%d{e}"), GNSSData.nmea_counters.gll);
+    WSContentSend_PD(PSTR("{s}&nbsp;&nbsp;• GSA Messages{m}%d{e}"), GNSSData.nmea_counters.gsa);
+    WSContentSend_PD(PSTR("{s}&nbsp;&nbsp;• GSV Messages{m}%d{e}"), GNSSData.nmea_counters.gsv);
+    WSContentSend_PD(PSTR("{s}&nbsp;&nbsp;• VTG Messages{m}%d{e}"), GNSSData.nmea_counters.vtg);
+    WSContentSend_PD(PSTR("{s}&nbsp;&nbsp;• Other NMEA Messages{m}%d{e}"), GNSSData.nmea_counters.other);
 #endif // USE_WEBSERVER
   }
 }
